a0ccc14bd76beee27d7beb9b8f9bde699909c91e
Bob Habig <rfhab2@gmail.com>
2018-01-30 16:21:45 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 const app = "I don't do much.";
+
+const digitalClock = {
+  time: Math.round(Date.now() / 1000),
+  startTicking: function() {
+    setInterval(() => this.time++, 1000);
+  }
+};
+
+const censor = (word, string) => {
+    debugger
+    string.replace(RegExp(word, 'g'), 'BLEEP')
+}
+
+const violenceCensor = censor.bind(null, 'violence')
+
+const drugsCensor = censor.bind(null, 'drugs')
